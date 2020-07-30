4ac9b23b18f6d35870e7cdf8b7ff954a1ea690a5
jason99chang <46538983+jason99chang@users.noreply.github.com>
2020-07-30 04:09:57 -0700

    created executable file but not working

--- main.py
+++ main.py
@@ -1,23 +1,35 @@
 import webbrowser
 from pynput.keyboard import Key, Controller
 import time
+import os
 
 links = open("links.txt")
+
 for x in links:
     x = x.rstrip()
     webbrowser.open(url=x)
 
 keyboard = Controller()
+
 numTabs = len(open("links.txt").readlines())
 
 links.close()
 
-for y in range(0, numTabs):
-    time.sleep(0.5)
-    keyboard.press(Key.cmd)
-    keyboard.press('w')
-    keyboard.release(Key.cmd)
-    keyboard.release('w')
+input("Press enter to continue...")
+if os.name == 'nt':
+    for y in range(0, numTabs):
+        time.sleep(0.5)
+        keyboard.press(Key.ctrl)
+        keyboard.press('w')
+        keyboard.release(Key.ctrl)
+        keyboard.release('w')
+else:
+    for y in range(0, numTabs):
+        time.sleep(0.5)
+        keyboard.press(Key.cmd)
+        keyboard.press('w')
+        keyboard.release(Key.cmd)
+        keyboard.release('w')
 
 print("Sucessfully clicked then closed all links")
 
